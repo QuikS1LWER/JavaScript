871dd8674558b7654ca97c839c39b64c14ce025b
Taras <t.kulias.m@gmail.comqwqq>
2025-04-02 19:55:33 +0200

    calcIf

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-let userName = 'Tarik';
-let userAge = 32;
-let userHight = 175;
-console.log(userName + ' is ' + userAge + ' years old and ' + userHight  ' hight');
-console.log(userName);
+function calc(operation, a, b) {
+  if (operation == "add") {
+    return (a + b);
+  } else if (operation == "multi") {
+    return (a * b);
+  } else if (operation == "subtract") {
+    return (a - b);
+  } else {
+    return "error"
+  }
+}
+console.log(calc("add", 2, 2));
