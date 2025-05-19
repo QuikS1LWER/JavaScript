8583d749f01033dda089aa7de2396fe4088b8dae
Taras <t.kulias.m@gmail.comqwqq>
2025-05-19 21:17:59 +0200

    toDoObjectMetod

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
-const list = {
-  "create a new practice task": "In Progress", 
-  "make a bed": "Done",
-  "write a post": "To Do",
+const todo = {
+  list: {"create a new practice task": "In Progress", "make a bed": "Done","write a post": "To Do",},
+  showList() {
+    console.log(this.list);
+  },
+  deleteTask(task) {
+    delete this.list[task];
+  },
+  changeStatus(task, status) {
+    if (!(task in this.list)) {
+      console.log("task not exist")
+    } else {
+      this.list[task] = status;
+    };
+  },
+  addTask(task) {
+    if (task in this.list) {
+      console.log("This task already exist")
+    } else if (task.length > 50) {
+      console.log("Task name is to long")
+    } else {
+      this.list[task] = "To Do";
+    };
+  }
 };
 
-function addTask(task) {
-  list[task] = "To Do";
-}
-
-function changeStatus(task, status) {
-  list[task] = status;
-}
-
-function deleteTask(task) {
-  delete list[task];
-}
-
-function showList() {
-  console.log(list);
-};
-
-addTask("pak staff");
-changeStatus("write a post", "Done");
-deleteTask("make a bed")
-showList();
+todo.addTask("pak staf");
+todo.changeStatus("write a post", "Done");
+todo.deleteTask("make a bed");
+todo.showList();
